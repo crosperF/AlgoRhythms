cce0f9a7aa0132dcaabd2ee45d3ef8ff8466ed44
crosper <jcrosperfonseka@gmail.com>
2025-09-09 13:34:14 +0530

    optimized

--- Algos/Sorting/bubble.py
+++ Algos/Sorting/bubble.py
@@ -3,11 +3,15 @@ a = [20, 3, 1, 59]
 
 
 def bubble_sort(arr):
-    for i in range(len(arr), -1, -1):
-        for j in range(i - 1):
+    for i in range(len(arr) - 1, 0, -1):
+        print(i)
+        swaps = False
+        for j in range(i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
+                swaps = True
+        if not swaps:
+            break
     return arr
 
 print(bubble_sort(a))
