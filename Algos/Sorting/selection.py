bb488ea2ac079e0d2eefdf2f7f84c026a7b23371
crosper <jcrosperfonseka@gmail.com>
2025-09-11 15:18:07 +0530

    selection sort

---
+++ Algos/Sorting/selection.py
@@ -0,0 +1,20 @@
+import re
+
+
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_idx = i
+
+        for j in range(i, n):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+
+        if min_idx != i:
+            arr[min_idx], arr[i] = arr[i], arr[min_idx]
+
+    return arr
+
+res = selection_sort([18, 39, 41, 2, 90, 1])
+print(res)
