1abd647809163d2c5348604e1a41492beb9d478d
crosper <jcrosperfonseka@gmail.com>
2025-09-17 20:12:33 +0530

    insertion

--- Algos/Sorting/insertion.py
+++ Algos/Sorting/insertion.py
@@ -6,7 +6,22 @@ def insertion_sort(arr):
             j -= 1
     return arr
 
+
+
+def insertion_sort_2 (arr):
+    for i in range(1, len(arr)):
+        j = i
+        val = arr[j]
+        idx_to_swap = j
+        while True:
+            if j == 0 or arr[j - 1] < val:
+                idx_to_swap = j - 1
+                break
+            j -= 1
+        arr[idx_to_swap], arr[j] = arr[j], arr[idx_to_swap]
+    return arr
+
 a = [12, 8, 4, 1]
-res = insertion_sort(a)
+res = insertion_sort_2(a)
 print(res)
 insertion_sort(a)
